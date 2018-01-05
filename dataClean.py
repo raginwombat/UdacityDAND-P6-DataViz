8c9b2c861fc0b7f38a0d9fb44c985e85eec04a7a
R N <rnelson03@gmail.com>
2018-01-05 04:34:23 -0800

    Updated datasets and added console timer

--- dataClean.py
+++ dataClean.py
@@ -5,7 +5,7 @@ FILENAME ='data/prosperLoanData.csv'
 
 data = pd.read_csv(FILENAME)
 
-seldata =  data[['ListingKey', 'ListingCreationDate', 'LoanOriginationQuarter', 'Occupation', 'BorrowerRate', 'IncomeRange']]
+seldata =  data[['ListingKey', 'ListingCreationDate',  'Occupation', 'BorrowerRate', 'IncomeRange']]
 
 #grpd = seldata.groupby(['LoanOriginationQuarter', 'Occupation']).agg('count')
 #grpd = seldata.groupby(['LoanOriginationQuarter']).count()
@@ -16,3 +16,5 @@ seldata =  data[['ListingKey', 'ListingCreationDate', 'LoanOriginationQuarter', 
 
 #grpd.to_csv('data/parsedData.csv')
 seldata.sample(n=1000).to_csv('data/smallParsedData.csv')
+seldata.sample(n=10000).to_csv('data/medParsedData.csv')
+seldata.to_csv('data/parsedData.csv')
